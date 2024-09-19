21142aa15e3d01b24c6a862227d151add17614e4
Sjibra10 <buhlebuka@gmail.com>
2024-09-19 08:52:21 +0000

    adding imports

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,67 +1,75 @@
 #!/usr/bin/env python3
-"""
-Route module for the API
-"""
+"""making main model for the API"""
+
 from os import getenv
-from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
+
 from flask_cors import (CORS, cross_origin)
-import os
+from flask import Flask, jsonify, abort, request
+from api.v1.views import app_views
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
 auth = None
-if os.getenv('AUTH_TYPE') == 'auth':
+
+if getenv('AUTH_TYPE') == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
-elif os.getenv('AUTH_TYPE') == 'basic_auth':
+elif getenv('AUTH_TYPE') == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 elif getenv('AUTH_TYPE') == 'session_auth':
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
-
-
-
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
-    """
-    return jsonify({"error": "Not found"}), 404
+elif getenv('AUTH_TYPE') == 'session_exp_auth':
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
+elif getenv('AUTH_TYPE') == 'session_db_auth':
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
 
 
 @app.errorhandler(401)
-def unauthorized_error(error):
-    """error handler"""
-    response = jsonify({'error': 'Unauthorized'})
-    response.status_code = 401
-    return response
+def unauthorized_error(error) -> str:
+    """handler 404 not found"""
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error) -> str:
-    """ Forbidden handler
-    """
+def forbidden_error(error) -> str:
+    """handler for forbidden"""
     return jsonify({"error": "Forbidden"}), 403
+    
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """handler 404 not found"""
+    return jsonify({"error": "Not found"}), 404
 
 
 @app.before_request
 def before_request() -> None:
-    """ Before request
-    """
-    paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-             '/api/v1/forbidden/', '/api/v1/auth_session/login/']
-    if not auth:
-        return None
-    if not auth.require_auth(request.path, paths):
-        return None
-    if not auth.authorization_header(request):
-        abort(401)
-    if not auth.current_user(request):
-        abort(403)
-    request.current_user = auth.current_user(request)
+    """request before"""
+    request_path_list = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/']
+
+    if auth:
+        if auth.require_auth(request.path, request_path_list):
+            if auth.authorization_header(
+                    request) is None and auth.session_cookie(request) is None:
+                abort(401)
+            request.current_user = auth.current_user(request)
+
+            if auth.current_user(request) is None:
+                abort(403)
+
+            if request.current_user is None:
+                abort(403)
 
 
 if __name__ == "__main__":
--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -1,63 +1,94 @@
 #!/usr/bin/env python3
-"""Module of Users views"""
-
-from flask import abort, jsonify, request
+""" Main base of Users views"""
 from api.v1.views import app_views
+from flask import abort, jsonify, request
 from models.user import User
 
+
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
-def view_all_users():
-    """GET /api/v1/users - Return a list of all User objects JSON represented"""
+def view_all_users() -> str:
+    """ list of all User objects"""
     all_users = [user.to_json() for user in User.all()]
     return jsonify(all_users)
 
+
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
-def view_one_user(user_id):
-    """GET /api/v1/users/:id - Return a User object JSON represented"""
+def view_one_user(user_id: str = None) -> str:
+    """User ID"""
+    if user_id is None:
+        abort(404)
     if user_id == 'me':
-        if not request.current_user:
+        if request.current_user is None:
             abort(404)
-        return jsonify(request.current_user.to_json())
+        else:
+            return jsonify(request.current_user.to_json())
 
     user = User.get(user_id)
-    if not user:
+    if user is None:
         abort(404)
+
     return jsonify(user.to_json())
 
+
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
-def delete_user(user_id):
-    """DELETE /api/v1/users/:id - Delete a User"""
+def delete_user(user_id: str = None) -> str:
+    """DELETE the User ID"""
+    if user_id is None:
+        abort(404)
     user = User.get(user_id)
-    if not user:
+    if user is None:
         abort(404)
     user.remove()
     return jsonify({}), 200
 
-@app_views.route('/users', methods=['POST'], strict_slashes=False)
-def create_user():
-    """POST /api/v1/users/ - Create a new User"""
-    data = request.get_json()
-    if not data or 'email' not in data or 'password' not in data:
-        return jsonify({'error': 'Invalid JSON'}), 400
-
-    user = User(**data)
-    try:
-        user.save()
-    except Exception as e:
-        return jsonify({'error': f'Can\'t create User: {str(e)}'}), 400
-
-    return jsonify(user.to_json()), 201
 
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
-def update_user(user_id):
-    """PUT /api/v1/users/:id - Update a User"""
-    user = User.get(user_id)
-    if not user:
+def update_user(user_id: str = None) -> str:
+    """ PUT to update the User"""
+    if user_id is None:
         abort(404)
-
-    data = request.get_json()
-    if not data:
-        return jsonify({'error': 'Invalid JSON'}), 400
-
-    user.update(data)
+    user = User.get(user_id)
+    if user is None:
+        abort(404)
+    rj = None
+    try:
+        rj = request.get_json()
+    except Exception as e:
+        rj = None
+    if rj is None:
+        return jsonify({'error': "Wrong format"}), 400
+    if rj.get('first_name') is not None:
+        user.first_name = rj.get('first_name')
+    if rj.get('last_name') is not None:
+        user.last_name = rj.get('last_name')
+    user.save()
     return jsonify(user.to_json()), 200
+
+
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def create_user() -> str:
+    """ POST to create the new User"""
+    rj = None
+    error_msg = None
+    try:
+        rj = request.get_json()
+    except Exception as e:
+        rj = None
+    if rj is None:
+        error_msg = "Wrong format"
+    if error_msg is None and rj.get("email", "") == "":
+        error_msg = "email missing"
+    if error_msg is None and rj.get("password", "") == "":
+        error_msg = "password missing"
+    if error_msg is None:
+        try:
+            user = User()
+            user.email = rj.get("email")
+            user.password = rj.get("password")
+            user.first_name = rj.get("first_name")
+            user.last_name = rj.get("last_name")
+            user.save()
+            return jsonify(user.to_json()), 201
+        except Exception as e:
+            error_msg = "Can't create User: {}".format(e)
+    return jsonify({'error': error_msg}), 400
